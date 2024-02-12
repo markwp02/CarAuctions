2f6dab93abc399401ef373455652107a82195440
Mark <markwp02@gmail.com>
2024-02-13 10:17:43 +1100

    End of Section 2

---
+++ src/AuctionService/Data/Migrations/20240205232543_AlteredAuctionsColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlteredAuctionsColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReservePricePrice",
+                table: "Auctions",
+                newName: "ReservePrice");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReservePrice",
+                table: "Auctions",
+                newName: "ReservePricePrice");
+        }
+    }
+}
