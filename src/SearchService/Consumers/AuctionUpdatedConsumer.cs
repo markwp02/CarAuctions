60cd3cec3715e1024e2e52106c8dc074cb4812c3
Mark <markwp02@gmail.com>
2024-02-19 12:13:12 +1100

    End of section 4

---
+++ src/SearchService/Consumers/AuctionUpdatedConsumer.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class AuctionUpdatedConsumer : IConsumer<AuctionUpdated>
+{
+    private readonly IMapper _mapper;
+
+    public AuctionUpdatedConsumer(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+    
+    public async Task Consume(ConsumeContext<AuctionUpdated> context)
+    {
+        Console.WriteLine("--> Consuming auction created: " + context.Message.Id);
+
+        var itemUpdated = _mapper.Map<Item>(context.Message);
+
+        await DB.Update<Item>()
+            .MatchID(itemUpdated.ID)
+            .ModifyOnly(a => new
+                {
+                    a.Make,
+                    a.Model,
+                    a.Year,
+                    a.Color,
+                    a.Mileage
+                }, itemUpdated)
+            .ExecuteAsync();    
+    }
+}
