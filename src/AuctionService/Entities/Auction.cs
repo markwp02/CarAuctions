af43d9b8542964d59d430161218ab9d81aab2f5f
Mark <markwp02@gmail.com>
2024-02-23 12:19:59 +1100

    End of Section 6

--- src/AuctionService/Entities/Auction.cs
+++ src/AuctionService/Entities/Auction.cs
@@ -8,8 +8,8 @@ public class Auction
     public int ReservePrice { get; set; } = 0;
     public string Seller { get; set; }
     public string Winner { get; set; }
-    public int SoldAmount {get; set; }
-    public int CurrentHighBid { get; set; }
+    public int? SoldAmount {get; set; }
+    public int? CurrentHighBid { get; set; }
     public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     public DateTime AuctionEnd { get; set;}
